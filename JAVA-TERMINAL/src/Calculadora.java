56780e22d09d7a59159ad889e4bab499e24d3e81
RafSanRib <rafasantosribeiro777@gmail.com>
2024-07-12 23:28:56 -0300

    Novas informações Java Terminal

--- JAVA-TERMINAL/src/Calculadora.java
+++ JAVA-TERMINAL/src/Calculadora.java
@@ -9,6 +9,17 @@
 * @version 1.0
 * @since   01/01/2022
 */
+/**
+* <h1>Calculadora</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Gleyson Sampaio
+* @version 1.0
+* @since   01/01/2022
+*/
 public class Calculadora {
     /**
    * Este método é utilizado para somar dois números inteiros
@@ -20,4 +31,3 @@ public class Calculadora {
         return  numeroUm + numeroDois;
     }
 }
-
